c3797542fd26ed8b3822af44340ad2b0f9b583e9
xuchang chen <imxuchang@gmail.com>
2017-02-18 00:23:31 -0800

    modify layout of all courses && add router for course detail component

--- src/component/dashboard/Dashboard.jsx
+++ src/component/dashboard/Dashboard.jsx
@@ -5,14 +5,11 @@ import Sidebar from './dashboard_sidebar';
 import Grid from 'react-bootstrap/lib/Grid';
 import Row from 'react-bootstrap/lib/Row';
 import Col from 'react-bootstrap/lib/Col';
+import dashboardStyle from 'style/dashboard.scss';
 
 export default class Dashboard extends React.Component {
   constructor() {
     super();
-
-    this.state = {
-
-    };
   }
 
   render() {
--- src/component/dashboard/dashboard_home_page.jsx
+++ src/component/dashboard/dashboard_home_page.jsx
@@ -26,60 +26,63 @@ export default class DashboardHomePage extends React.Component {
     }
   }
 
+  showLastActivity() {
+    var lastActivity = this.state.user.lastActivity;
+    if (lastActivity != null) {
+      return (
+        <div className="dashboardContentPanel dashboardContentPanelBorder">
+          <h1 className="dashboardContentPanelHeader">
+            <span>
+              <strong>
+                { this.state.user.lastActivity.courseName }
+              </strong>
+            </span>
+          </h1>
+          <a className="panelButton" href="">继续学习</a>
+          <p >You are on { this.state.user.lastActivity.videoName }</p>
+        </div>
+      );
+    }
+  }
+
+  showEnrolledCourses() {
+    var user = this.state.user;
+    if (user != null) {
+      var enrolledCourseInfo =  this.state.user.course;
+      enrolledCourseInfo = enrolledCourseInfo.map((c, i) => {
+        return (
+          <div
+            key={ c.courseName }
+            className="dashboardContentPanel dashboardContentPanelBorder dashboardContentCoursePanel"
+          >
+            <div><img className="coursePanelIcon" src={ courseIcon }/></div>
+            <div>
+              <h3 className="dashboardContentHeaderInPanel">{ c.courseName }</h3>
+              <p>Course Description { i }</p>
+            </div>
+            <div>
+              <a className="panelButton" href="">查看课程</a>
+            </div>
+          </div>
+        );
+      });
+    }
+  }
+
   render() {
     console.log(this.state.user);
-    if (this.state.user != null) {
-      var lastActivity = this.state.user.lastActivity;
-      var enrolledCourseInfo =  this.state.user.course;
-      var lastActivityElem = [];
-      var enrolledCourses = [];
-
-      if (lastActivity != null) {
-        lastActivityElem.push(
-            <div className="dashboardContentPanel dashboardContentPanelBorder">
-              <h1 className="dashboardContentPanelHeader">
-                <span>
-                  <strong>
-                    { this.state.user.lastActivity.courseName }
-                  </strong>
-                </span>
-              </h1>
-              <a className="panelButton" href="">继续学习</a>
-              <p >You are on { this.state.user.lastActivity.videoName }</p>
-            </div>
-        );
-      }
-
-      if (enrolledCourseInfo != null) {
-        for (var i = 0;i < enrolledCourseInfo.length;i++) {
-          enrolledCourses.push(
-            <div className="dashboardContentPanel dashboardContentPanelBorder dashboardContentCoursePanel">
-              <div><img className="coursePanelIcon" src={ courseIcon }/></div>
-              <div>
-                <h3 className="dashboardContentHeaderInPanel">{ this.state.user.course[i].courseName }</h3>
-                <p >Course Description { i }</p>
-              </div>
-              <div>
-                <a className="panelButton" href="">查看课程</a>
-              </div>
-            </div>
-          );
-        }
-      }
-    }
-
     return (
       <div className="container">
         <div id="dashboardContentLastActivity">
           <h3 className="dashboardContentHeader">最近动态</h3>
           <div>
-            { lastActivityElem }
+            { this.showLastActivity() }
           </div>
         </div>
         <div>
           <h3 className="dashboardContentHeader">我的课程</h3>
           <div>
-            { enrolledCourses }
+            { this.showEnrolledCourses() }
           </div>
         </div>
       </div>
