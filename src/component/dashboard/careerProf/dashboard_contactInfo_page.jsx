f7be642b538af6a741ed773f034fb2231fffc422
BrandonQi <bqi@stevens.edu>
2017-02-28 20:09:45 -0500

    profile setting UI change

--- src/component/dashboard/careerProf/dashboard_contactInfo_page.jsx
+++ src/component/dashboard/careerProf/dashboard_contactInfo_page.jsx
@@ -4,6 +4,8 @@ import LoginStore from 'store/login';
 import Form from 'react-bootstrap/lib/Form';
 import FormGroup from 'react-bootstrap/lib/FormGroup';
 import Col from 'react-bootstrap/lib/Col';
+import Row from 'react-bootstrap/lib/Row';
+import Grid from 'react-bootstrap/lib/Grid';
 import FormControl from 'react-bootstrap/lib/FormControl';
 import Button from 'react-bootstrap/lib/Button';
 import ControlLabel from 'react-bootstrap/lib/ControlLabel';
@@ -29,87 +31,100 @@ export default class DashboardContactInfoPage extends React.Component {
 
   render() {
     return (
-        <Form horizontal>
-          <FormGroup controlId="formHorizontalEmail">
-            <Col componentClass={ControlLabel} sm={2}>
-              名
-            </Col>
-            <Col sm={10}>
-              <FormControl />
-            </Col>
-          </FormGroup>
+        <Grid>
+          <Col xs={1} md={1}></Col>
+          <Col xs={12} md={8}>
+            <Form horizontal>
+              <Row>
+                <Col xs={9} md={6}>
+                  <FormGroup controlId="formHorizontalEmail">
+                    <Col componentClass={ControlLabel} sm={2}>
+                      名
+                    </Col>
+                    <Col sm={10}>
+                      <FormControl />
+                    </Col>
+                  </FormGroup>
+                </Col>
+                <Col xs={9} md={6}>
+                  <FormGroup controlId="formHorizontalPassword">
+                    <Col componentClass={ControlLabel} sm={2}>
+                      姓
+                    </Col>
+                    <Col sm={10}>
+                      <FormControl />
+                    </Col>
+                  </FormGroup>
+                </Col>
+              </Row>
 
-          <FormGroup controlId="formHorizontalPassword">
-            <Col componentClass={ControlLabel} sm={2}>
-              姓
-            </Col>
-            <Col sm={10}>
-              <FormControl />
-            </Col>
-          </FormGroup>
-
-          <FormGroup controlId="formHorizontalEmail">
-            <Col componentClass={ControlLabel} sm={2}>
-              邮件
-            </Col>
-            <Col sm={10}>
-              <FormControl />
-            </Col>
-          </FormGroup>
-
-          <FormGroup controlId="formHorizontalEmail">
-            <Col componentClass={ControlLabel} sm={2}>
-              地址
-            </Col>
-            <Col sm={10}>
-              <FormControl />
-            </Col>
-          </FormGroup>
-
-          <FormGroup controlId="formHorizontalEmail">
-            <Col componentClass={ControlLabel} sm={2}>
-              国家
-            </Col>
-            <Col sm={10}>
-              <FormControl />
-            </Col>
-          </FormGroup>
-
-          <FormGroup controlId="formHorizontalEmail">
-            <Col componentClass={ControlLabel} sm={2}>
-              邮编
-            </Col>
-            <Col sm={10}>
-              <FormControl />
-            </Col>
-          </FormGroup>
-
-          <FormGroup controlId="formHorizontalEmail">
-            <Col componentClass={ControlLabel} sm={2}>
-              移动电话
-            </Col>
-            <Col sm={10}>
-              <FormControl />
-            </Col>
-          </FormGroup>
-
-          <FormGroup controlId="formHorizontalEmail">
-            <Col componentClass={ControlLabel} sm={2}>
-              固话
-            </Col>
-            <Col sm={10}>
-              <FormControl />
-            </Col>
-          </FormGroup>
-
-          <FormGroup>
-            <Col smOffset={2} sm={10}>
-              <Button type="submit">
-                保存
-              </Button>
-            </Col>
-          </FormGroup>
-        </Form>
+              <Row>
+                <Col xs={9} md={6}>
+                  <FormGroup controlId="formHorizontalEmail">
+                    <Col componentClass={ControlLabel} sm={2}>
+                      地址
+                    </Col>
+                    <Col sm={10}>
+                      <FormControl />
+                    </Col>
+                  </FormGroup>
+                </Col>
+                <Col xs={9} md={6}>
+                  <FormGroup controlId="formHorizontalEmail">
+                    <Col componentClass={ControlLabel} sm={2}>
+                      城市
+                    </Col>
+                    <Col sm={10}>
+                      <FormControl />
+                    </Col>
+                  </FormGroup>
+                </Col>
+              </Row>
+              <Row className="dashboardContentOneLineControlRow">
+                <FormGroup controlId="formHorizontalEmail">
+                  <Col componentClass={ControlLabel} sm={2}>
+                    邮件
+                  </Col>
+                  <Col sm={10}>
+                    <FormControl />
+                  </Col>
+                </FormGroup>
+                <FormGroup controlId="formHorizontalEmail">
+                  <Col componentClass={ControlLabel} sm={2}>
+                    邮编
+                  </Col>
+                  <Col sm={10}>
+                    <FormControl />
+                  </Col>
+                </FormGroup>
+                <FormGroup controlId="formHorizontalEmail">
+                  <Col componentClass={ControlLabel} sm={2}>
+                    移动电话
+                  </Col>
+                  <Col sm={10}>
+                    <FormControl />
+                  </Col>
+                </FormGroup>
+                <FormGroup controlId="formHorizontalEmail">
+                  <Col componentClass={ControlLabel} sm={2}>
+                    固话
+                  </Col>
+                  <Col sm={10}>
+                    <FormControl />
+                  </Col>
+                </FormGroup>
+                <FormGroup>
+                  <Col smOffset={2} sm={10}>
+                    <Button type="submit">
+                      保存
+                    </Button>
+                  </Col>
+                </FormGroup>
+              </Row>
+            </Form>
+          </Col>
+          <Col xs={5} md={3}></Col>
+        </Grid>
     );
   };
 }
--- src/component/dashboard/careerProf/dashboard_education_page.jsx
+++ src/component/dashboard/careerProf/dashboard_education_page.jsx
@@ -4,6 +4,8 @@ import LoginStore from 'store/login';
 import Form from 'react-bootstrap/lib/Form';
 import FormGroup from 'react-bootstrap/lib/FormGroup';
 import Col from 'react-bootstrap/lib/Col';
+import Row from 'react-bootstrap/lib/Row';
+import Grid from 'react-bootstrap/lib/Grid';
 import FormControl from 'react-bootstrap/lib/FormControl';
 import Button from 'react-bootstrap/lib/Button';
 import ControlLabel from 'react-bootstrap/lib/ControlLabel';
@@ -29,89 +31,93 @@ export default class DashboardEducationPage extends React.Component {
 
   render() {
     return (
-      <div>
-        <div>
-        <h3 className="dashboardContentChineseHead">教育背景:</h3>
-        <div>内容为空</div>
-        </div>
-        <div>
-          <h3 className="dashboardContentChineseHead">添加教育经历:</h3>
-          <div>
-            <Form horizontal>
-              <FormGroup controlId="formHorizontalEmail">
-                <Col componentClass={ControlLabel} sm={2}>
-                  学校
-                </Col>
-                <Col sm={10}>
-                  <FormControl />
-                </Col>
-              </FormGroup>
+      <Grid>
+        <Col xs={1} md={1}></Col>
+        <Col xs={12} md={8}>
+          <Row>
+          <h3 className="dashboardContentChineseHead">教育背景:</h3>
+          <div>内容为空</div>
+          </Row>
+          <Row>
+            <h3 className="dashboardContentChineseHead">添加教育经历:</h3>
+            <Row>
+              <Form horizontal>
+                <FormGroup controlId="formHorizontalEmail">
+                  <Col componentClass={ControlLabel} sm={2}>
+                    学校
+                  </Col>
+                  <Col sm={10}>
+                    <FormControl />
+                  </Col>
+                </FormGroup>
 
-              <FormGroup controlId="formHorizontalEmail">
-                <Col componentClass={ControlLabel} sm={2}>
-                  地址
-                </Col>
-                <Col sm={10}>
-                  <FormControl />
-                </Col>
-              </FormGroup>
+                <FormGroup controlId="formHorizontalEmail">
+                  <Col componentClass={ControlLabel} sm={2}>
+                    地址
+                  </Col>
+                  <Col sm={10}>
+                    <FormControl />
+                  </Col>
+                </FormGroup>
 
-              <FormGroup controlId="formHorizontalEmail">
-                <Col componentClass={ControlLabel} sm={2}>
-                  开始时间
-                </Col>
-                <Col sm={10}>
-                  <FormControl />
-                </Col>
-              </FormGroup>
+                <FormGroup controlId="formHorizontalEmail">
+                  <Col componentClass={ControlLabel} sm={2}>
+                    开始时间
+                  </Col>
+                  <Col sm={10}>
+                    <FormControl />
+                  </Col>
+                </FormGroup>
 
-              <FormGroup controlId="formHorizontalEmail">
-                <Col componentClass={ControlLabel} sm={2}>
-                  结束时间
-                </Col>
-                <Col sm={10}>
-                  <FormControl />
-                </Col>
-              </FormGroup>
+                <FormGroup controlId="formHorizontalEmail">
+                  <Col componentClass={ControlLabel} sm={2}>
+                    结束时间
+                  </Col>
+                  <Col sm={10}>
+                    <FormControl />
+                  </Col>
+                </FormGroup>
 
-              <FormGroup controlId="formHorizontalPassword">
-                <Col componentClass={ControlLabel} sm={2}>
-                  学位
-                </Col>
-                <Col sm={10}>
-                  <FormControl />
-                </Col>
-              </FormGroup>
+                <FormGroup controlId="formHorizontalPassword">
+                  <Col componentClass={ControlLabel} sm={2}>
+                    学位
+                  </Col>
+                  <Col sm={10}>
+                    <FormControl />
+                  </Col>
+                </FormGroup>
 
-              <FormGroup controlId="formHorizontalEmail">
-                <Col componentClass={ControlLabel} sm={2}>
-                  专业
-                </Col>
-                <Col sm={10}>
-                  <FormControl componentClass="textarea"/>
-                </Col>
-              </FormGroup>
+                <FormGroup controlId="formHorizontalEmail">
+                  <Col componentClass={ControlLabel} sm={2}>
+                    专业
+                  </Col>
+                  <Col sm={10}>
+                    <FormControl />
+                  </Col>
+                </FormGroup>
 
-              <FormGroup controlId="formHorizontalEmail">
-                <Col componentClass={ControlLabel} sm={2}>
-                描述
-                </Col>
-                <Col sm={10}>
-                  <FormControl componentClass="textarea"/>
-                </Col>
-              </FormGroup>
+                <FormGroup controlId="formHorizontalEmail">
+                  <Col componentClass={ControlLabel} sm={2}>
+                  描述
+                  </Col>
+                  <Col sm={10}>
+                    <FormControl componentClass="textarea"/>
+                  </Col>
+                </FormGroup>
 
-              <FormGroup>
-                <Col smOffset={2} sm={10}>
-                  <Button type="submit">
-                    添加教育经历
-                  </Button>
-                </Col>
-              </FormGroup>
-            </Form>     
-          </div>
-        </div>
-      </div>
+                <FormGroup>
+                  <Col smOffset={2} sm={10}>
+                    <Button type="submit">
+                      添加教育经历
+                    </Button>
+                  </Col>
+                </FormGroup>
+              </Form>     
+            </Row>
+          </Row>
+        </Col>
+        <Col xs={5} md={3}></Col>
+      </Grid>
     );
   };
 }
