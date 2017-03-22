6db21d60418aa5391c1a9798294828796b77814b
xuchangchen <imxuchang@gmail.com>
2017-03-22 00:35:17 -0700

    create client side page for verify success

--- src/index.jsx
+++ src/index.jsx
@@ -11,7 +11,6 @@ import ContactMethod from 'component/main/contactMethod.jsx';
 import JoinUs from 'component/main/joinUs.jsx';
 import ProfessorsIntro from 'component/main/professorsIntro.jsx';
 import CompanyIntro from 'component/main/companyIntro.jsx';
-import FindMyPassword from 'component/password/find_password.jsx';
 import NotFoundPage from 'component/main/not_found_page.jsx';
 import VerifyEmail from 'component/main/verify_email.jsx';
 import NavbarComponent from 'component/main/navbar.jsx';
@@ -20,24 +19,25 @@ import Agreement from 'component/main/agreement.jsx';
 import TrainingS from 'component/main/trainingS.jsx';
 import Activities from 'component/main/activities.jsx';
 
+// Password
+import FindMyPassword from 'component/password/find_password.jsx';
+import VerifySuccess from 'component/password/verify_success.jsx';
+
 // Dashboard Components
 import Dashboard from 'component/dashboard/Dashboard.jsx';
 import DashboardHomePage from 'component/dashboard/dashboard_home_page.jsx';
 import DashboardCoursesPage from 'component/dashboard/dashboard_courses_page.jsx';
 import DashboardLecturesPage from 'component/dashboard/dashboard_lecture_page.jsx';
-
 import DashboardCareerOpporPage from 'component/dashboard/dashboard_careerOppor_page.jsx';
 import DashboardCareerInfoPage from 'component/dashboard/dashboard_careerInfo_page.jsx';
 import DashboardChartPage from 'component/dashboard/dashboard_chart_page.jsx';
 import DashboardSettingPage from 'component/dashboard/dashboard_setting_page.jsx';
-
 import DashboardSummaryPage from 'component/dashboard/careerProf/dashboard_summary_page.jsx';
 import DashboardContactInfoPage from 'component/dashboard/careerProf/dashboard_contactInfo_page.jsx';
 import DashboardExperiencePage from 'component/dashboard/careerProf/dashboard_experience_page.jsx';
 import DashboardEducationPage from 'component/dashboard/careerProf/dashboard_education_page.jsx';
 import DashboardResumePage from 'component/dashboard/careerProf/dashboard_resume_page.jsx';
 import DashboardBasicInfoPage from 'component/dashboard/careerProf/dashboard_basic_info_page.jsx';
-
 import DashboardCourseDetail from 'component/dashboard/dashboard_course_detail.jsx';
 import DashboardPlayVideo from 'component/dashboard/dashboard_play_video.jsx';
 
@@ -63,8 +63,16 @@ ReactDOM.render(
           } 
         }
       />
-      <Route path='/verifyEmail' components={{ navbar: NavbarComponent, body: VerifyEmail }} />
-      <Route path='/reset_password' components={{ navbar: NavbarComponent, body: FindMyPassword }} />
+      <Route path='/verifyEmail' components={{ navbar: NavbarComponent, body: VerifyEmail, footer: BottomNavbarComponent }} />
+      <Route path='/reset_password' components={{ navbar: NavbarComponent, body: FindMyPassword, footer: BottomNavbarComponent }} />
+      <Route
+        path='/verify/:token'
+        getComponents={
+          (nextState, cb) => {
+            cb(null, { navbar: NavbarComponent, body: VerifySuccess, footer: BottomNavbarComponent  });
+          } 
+        }
+      />
     </Route>
 
     <Route path='/dashboard' component={ Dashboard } >
