30290bbe4b2d01f43be258b931a450964213e00d
xuchangchen <imxuchang@gmail.com>
2017-05-29 21:24:13 -0700

    login/register in video play page

--- src/component/dashboard/dashboard_play_video.jsx
+++ src/component/dashboard/dashboard_play_video.jsx
@@ -2,12 +2,15 @@ import config from 'root/config.json';
 import React from 'react';
 import LoginStore from 'store/login';
 import VideoStore from 'store/video';
-import courseStyle from 'style/course.scss';
 import { browserHistory } from 'react-router';
 import Col from 'react-bootstrap/lib/Col';
 import Row from 'react-bootstrap/lib/Row';
+import Modal from 'react-bootstrap/lib/Modal';
 import Button from 'react-bootstrap/lib/Button';
+import LoginRegisterTabs from '../main/login_register_tabs';
 import FaAngleLeft from 'react-icons/lib/fa/angle-left';
+import LogoImage from 'style/asset/logo.png';
+import 'style/course.scss';
 
 export default class DashboardPlayVideo extends React.Component {
   constructor(props) {
@@ -15,10 +18,15 @@ export default class DashboardPlayVideo extends React.Component {
     this.state = {
       videoName: props.params.videoName,
       videoDescription: '',
-      vedioPath: null,
       error: null,
+      showModal: false,
+      loginTab: true,
+      user: null,
     };
     this.handleError = this.handleError.bind(this);
+    this.openLogin = this.openLogin.bind(this);
+    this.openRegister = this.openRegister.bind(this);
+    this.close = this.close.bind(this);
   }
 
   componentDidMount() {
@@ -29,6 +37,10 @@ export default class DashboardPlayVideo extends React.Component {
         videoDescription: video.description
       });
     });
+
+    LoginStore.subscribe(() => {
+      this.setState({ user: LoginStore.getState() });
+    });
   }
 
   handleError(event) {
@@ -38,39 +50,65 @@ export default class DashboardPlayVideo extends React.Component {
     }
   }
 
+  openLogin() {
+    this.setState({ showModal: true , loginTab: true });
+  }
+
+  openRegister() {
+    this.setState({ showModal: true, loginTab: false });
+  }
+
+  close() {
+    this.setState({ showModal: false }); 
+  }
+
   render() {
     return (
       <div>
-        <Row className="textCenter">
+        <Row className="textCenter" style={{ marginBottom: 30 }}>
           <h3>{ this.state.videoName }</h3>
         </Row>
-        <Row className="textCenter">
-          {
-            this.state.error ?
-            <div style={{ position: 'relative', top: 400, opacity: 0.6 }}>
-              <h4>无法播放该视频，可能由于以下原因：</h4>
-              <ul style={{ listStyle: 'none' }}>
-                <li>你尚未购买该视频</li>
-                <li>视频播放错误</li>
-                <li>网络不稳定</li>
-              </ul>
+        <Row>
+          <Row className="textCenter" style={{ position: 'relative' }}>
+            <video
+              className="videoPlayer"
+              src={ `http://${config.host}:${config.rest_port}/api/v1/display/${this.state.videoName}` }
+              onError={ this.handleError }
+              controls
+              autoPlay
+            />
+            {
+              this.state.error ?
+              <div className="videoWarning">
+                <h4>无法播放该视频，可能由于以下原因：</h4>
+                {
+                  this.state.user ?
+                  <ul>
+                    <li>你尚未购买该视频</li>
+                    <li>网络不稳定</li>
+                  </ul>
+                  :
+                  <ul>
+                    <li>你尚未<a href="#" onClick={ this.openLogin }>登陆</a></li>
+                  </ul>
+                }
+              </div>
+              : ''
+            }
+          </Row>
+          <Row className="videoDescription">
+            <p>{ this.state.videoDescription }</p>
+          </Row>
+        </Row>
+        <Modal show={ this.state.showModal } onHide={ this.close }>
+            <div className="textCenter">
+              <a href="#"><img id="loginSporitLogo" src={ LogoImage }/></a>
             </div>
-            :
-            ''
-          }
-          <video 
-            className="videoPlayer" 
-            src={ `http://${config.host}:${config.rest_port}/api/v1/display/${this.state.videoName}` }
-            onError={ this.handleError }
-            controls
-            autoPlay
-          >
-          </video>
-        </Row>
-        <Row className="videoDescription">
-          <h3>相关介绍</h3>
-          <p>{ this.state.videoDescription }</p>
-        </Row>
+            <button type="button" className="modalCloseButton" onClick={ this.close }>×</button>
+          <Modal.Body>
+            <LoginRegisterTabs needReload={ true } closeModalWindow={ this.close } showLoginTab={ this.state.loginTab }/>
+          </Modal.Body>
+        </Modal>
       </div>
     );
   };
